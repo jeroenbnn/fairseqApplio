dc514cd655a8a33c1b76c47a58b92da36ccded13
Jeroen Boonen <76564930+jeroenbnn@users.noreply.github.com>
2024-08-01 20:13:13 +0200

    Update model_utils.py

--- fairseq/models/model_utils.py
+++ fairseq/models/model_utils.py
@@ -82,7 +82,7 @@ def fill_tensors(
         x = expand_2d_or_3d_tensor(x, y.size(1), padding_idx)
         x[mask] = y
     elif x.size(1) > y.size(1):
-        #x[mask] = torch.tensor(padding_idx).type_as(x)
+        x[mask] = torch.tensor(padding_idx).type_as(x)
         if x.dim() == 2:
             x[mask, : y.size(1)] = y
         else:
